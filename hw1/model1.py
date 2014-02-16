8b5d1289ae6e973cf5f25610f7c1d14c468d154b
Prashant Sridhar <psridha1@andrew.cmu.edu>
2014-02-15 20:33:50 -0500

    pre merge

--- hw1/model1.py
+++ hw1/model1.py
@@ -12,18 +12,14 @@ def viterbi(trans_p, start_p, emit_p, target, source):
     for y in source:
         V[0][y] = start_p[y] * emit_p[y][target[0]]
         paths[y] = [y]
- 
-    # alternative Python 2.7+ initialization syntax
-    # V = [{y:(start_p[y] * emit_p[y][obs[0]]) for y in states}]
-    # path = {y:[y] for y in states}
- 
+
     # Run Viterbi for t > 0
     for t in range(1, len(target)):
         V.append({})
         newpaths = {}
  
         for y in source:
-            (prob, state) = max((V[t-1][y0] * trans_p[y0][y] * emit_p[y][obs[t]], y0) for y0 in source)
+            (prob, state) = max((V[t-1][y0] * trans_p[y0][y] * emit_p[y][target[t]], y0) for y0 in source)
             V[t][y] = prob
             newpaths[y] = paths[state] + [y]
  
