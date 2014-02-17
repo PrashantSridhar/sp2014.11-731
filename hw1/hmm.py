8c30cc0598cd6add75479ff317c9d8046f20d677
akcar <alokkoth@lagos.lti.cs.cmu.edu>
2014-02-17 14:02:21 -0500

    hmm nearing completition

--- hw1/hmm.py
+++ hw1/hmm.py
@@ -43,32 +43,38 @@ def baum_welch(source_corpus,target_corpus):
         source_sentence=source_corpus[k]
         target_sentence=target_corpus[k]
         fwd,bkw,posterior=fwd_bkw(source_sentence, states,start_probability,alignment,translation, end_st)
-
-        for t in range(target):
-            psi[t]={}
+        #can be simplified, just computer corpus_psi[i,j]. gamma_psi[i]=sum corpus_psi[,j]:
+        for t,tt in enumerate(target):
+            psi={}
+            #psi[t]={}
             sum_psi=0.0
             for i,si in enumerate(source):
+                fparamdenom=float(sum(t.get((si,tt),float(1.0/corpus_length)) for tword in target_sentence))
                 for j,sj in enumerate(source):
-                    psi[t][(i,j)]=fwd[i][t]*align[j-i]*trans[source[j]][target[t+1]]*bkw[j][t+1]
-                    sum_psi+=psi[t][(i,j)]
+                    psi[(i,j)]=fwd[i][t]*align[j-i]*trans[source[j]][target[t+1]]*bkw[j][t+1]
+                    sum_psi+=psi[(i,j)]
             for i,si in enumerate(source):
                 for j,sj in enumerate(source):
-                    psi[t][(i,j)]=psi[t][(i,j)]/sum_psi
-                    corpus_psi[t][(i,j)]+=psi[t][(i,j)]
-                    gamma[t][i]+=psi[t][(i,j)]
-                corpus_gamma[t][i]+=gamma[t][i]
+                    psi[(i,j)]=psi[(i,j)]/sum_psi
+                    corpus_psi[(i,j)]+=psi[(i,j)]
 
     ##UPDATING##
-    for k in range(len(source_corpus)):
-    for i,si in enumerate(source):
-        for j,sj in enumerate(source):
-            for t in range(target):
-                num+=psi[t][(i,j)]
-        for t,st in enumerate(target):
-            den+=gamma[t][i]
-            t[si][st]=gamma[t][i]
-        for j,sj in enumerate(source):
-            align[j-i]=num/den
+    for key in corpus_psi.keys():
+        if key[0] not in gamma:
+            gamma[key[0]]=0.0
+        gamma[key[0]]+=corpus_psi[key] 
+    for key in corpus_psi.keys():
+        jump=key[1]-key[0]
+        '''
+        if jump not in align_num:
+            align_num[jump]=0.0
+        if jump not in align_den:
+            align_den[jump]=0.0
+        '''
+        align[jump]+=corpus_psi[key]/gamma[key[0]] #adding each aij seperately, v.s.adding all numerators and denominators.
+    normalize_jump=sum(align)
+    align=[float(align[jump])/float(normalize_jump) for jump in align]
+           
 
             
             
